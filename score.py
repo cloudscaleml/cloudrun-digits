109b2538fd4cd7ed51de07aacf9510acc3415173
sethjuarez <me@sethjuarez.com>
2019-07-19 13:54:55 -0700

    added unfinished score file

--- score.py
+++ score.py
@@ -1,9 +1,11 @@
 import json
 import time
+import requests
 import datetime
 import numpy as np
+from PIL import Image
+from io import BytesIO
 import tensorflow as tf
-from io import StringIO
 
 from azureml.core.model import Model
 
@@ -11,52 +13,78 @@ def init():
     global model
 
     try:
-        model_path = Model.get_model_path('SimpleMNIST')
+        model_path = Model.get_model_path('tacosandburritos')
     except:
-        model_path = 'outputs/digits.pb'
+        model_path = '../../data/model/latest.h5'
 
-    with tf.gfile.GFile(model_path, "rb") as f:
-        graph_def = tf.GraphDef()
-        graph_def.ParseFromString(f.read())
-
-    with tf.Graph().as_default() as graph:
-        tf.import_graph_def(graph_def, name='simplemnist')
+    print('Attempting to load model')
+    model = tf.keras.models.load_model(model_path)
+    model.summary()
+    print('Done!')
 
     print('Initialized model "{}" at {}'.format(model_path, datetime.datetime.now()))
-    model = graph
 
 def run(raw_data):
     global model
     prev_time = time.time()
           
     post = json.loads(raw_data)
-    # load and normalize image
-    image = np.loadtxt(StringIO(post['image']), delimiter=',') / 255.
-
-    x = model.get_tensor_by_name('simplemnist/x:0')
-    y = model.get_tensor_by_name('simplemnist/Model/prediction:0')
-
-    with tf.Session(graph=model) as sess:
-        pred = sess.run(y, feed_dict={ x: image.reshape(1, 784) })[0]
+    img_path = post['image']
 
     current_time = time.time()
-    inference_time = datetime.timedelta(seconds=current_time - prev_time)
 
+    tensor = process_image(img_path, 160)
+    t = tf.reshape(tensor, [-1, 160, 160, 3])
+    o = model.predict(t, steps=1)#[0][0]
+    print(o)
+    o = o[0][0]
+    inference_time = datetime.timedelta(seconds=current_time - prev_time)
     payload = {
         'time': inference_time.total_seconds(),
-        'prediction': int(np.argmax(pred)),
-        'scores': pred.tolist()
+        'prediction': 'burrito' if o < 0.5 else 'tacos',
+        'scores': str(o)
     }
 
     print('Input ({}), Prediction ({})'.format(post['image'], payload))
 
     return payload
 
+def process_image(path, image_size):
+    # Extract image (from web or path)
+    if(path.startswith('http')):
+        response = requests.get(path)
+        img = np.array(Image.open(BytesIO(response.content)))
+    else:
+        img = np.array(Image.open(path))
+
+    img_tensor = tf.convert_to_tensor(img, dtype=tf.float32)
+    #tf.image.decode_jpeg(img_raw, channels=3)
+    img_final = tf.image.resize(img_tensor, [image_size, image_size]) / 255
+    return img_final
+    
+def info(msg, char = "#", width = 75):
+    print("")
+    print(char * width)
+    print(char + "   %0*s" % ((-1*width)+5, msg) + char)
+    print(char * width)
+
 if __name__ == "__main__":
-    img = '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,67,232,39,0,0,0,0,0,0,0,0,0,62,81,0,0,0,0,0,0,0,0,0,0,0,0,0,0,120,180,39,0,0,0,0,0,0,0,0,0,126,163,0,0,0,0,0,0,0,0,0,0,0,0,0,2,153,210,40,0,0,0,0,0,0,0,0,0,220,163,0,0,0,0,0,0,0,0,0,0,0,0,0,27,254,162,0,0,0,0,0,0,0,0,0,0,222,163,0,0,0,0,0,0,0,0,0,0,0,0,0,183,254,125,0,0,0,0,0,0,0,0,0,46,245,163,0,0,0,0,0,0,0,0,0,0,0,0,0,198,254,56,0,0,0,0,0,0,0,0,0,120,254,163,0,0,0,0,0,0,0,0,0,0,0,0,23,231,254,29,0,0,0,0,0,0,0,0,0,159,254,120,0,0,0,0,0,0,0,0,0,0,0,0,163,254,216,16,0,0,0,0,0,0,0,0,0,159,254,67,0,0,0,0,0,0,0,0,0,14,86,178,248,254,91,0,0,0,0,0,0,0,0,0,0,159,254,85,0,0,0,47,49,116,144,150,241,243,234,179,241,252,40,0,0,0,0,0,0,0,0,0,0,150,253,237,207,207,207,253,254,250,240,198,143,91,28,5,233,250,0,0,0,0,0,0,0,0,0,0,0,0,119,177,177,177,177,177,98,56,0,0,0,0,0,102,254,220,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,254,137,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,254,57,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,254,57,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,255,94,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,254,96,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,254,153,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,169,255,153,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,96,254,153,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0'
-    data = {
-        'image': img
+    images = {
+        'tacos': 'https://c1.staticflickr.com/5/4022/4401140214_f489c708f0_b.jpg',
+        'burrito': 'https://www.exploreveg.org/files/2015/05/sofritas-burrito.jpeg'
     }
 
     init()
-    out = run(json.dumps(data))
+
+    for k, v in images.items():
+        print('{} => {}'.format(k, v))
+
+    info('Taco Test')
+    taco = json.dumps({ 'image': images['tacos'] })
+    print(taco)
+    run(taco)
+
+    info('Burrito Test')
+    burrito = json.dumps({ 'image': images['burrito'] })
+    print(burrito)
+    run(burrito)
